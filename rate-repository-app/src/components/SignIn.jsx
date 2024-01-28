c3c2d4fa9d3055075861be937ad15646befe5945
Learner Getready <learnergit@hymyilee.fi>
2024-01-28 12:56:38 +0200

    10.8

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,41 @@
-import Text from './Text';
+import FormikTextInput from "./FormikTextInput";
+import { Pressable, View } from "react-native";
+import { Formik } from "formik";
+import Text from "./Text";
+import theme from "../theme";
+
+const initialValues = {
+    username: "",
+    password: ""
+};
+
+const SignInForm = ({ onSubmit }) => {
+    return(
+        <View>
+            <FormikTextInput name="username" placeholder="username" />
+            <FormikTextInput name="password" placeholder="password" secureTextEntry />
+            <Pressable onPress={onSubmit} style={{ backgroundColor: theme.colors.primary, padding: 10, borderRadius: 5, margin: 10 }}>
+                <Text style={{ color: "white", textAlign: "center" }} >Sign in</Text>
+            </Pressable>
+        </View>
+    );
+};
+
 
 const SignIn = () => {
-    return <Text>The sign-in view</Text>;
+
+    const onSubmit = (values, formikBag) => {
+        console.log(values);
+        formikBag.resetForm();
+    };
+
+    return (
+        <View>
+            <Formik initialValues={initialValues} onSubmit={onSubmit}>
+                { ({ handleSubmit }) => <SignInForm onSubmit={handleSubmit}/>}
+            </Formik>
+        </View>
+    );
 };
 
 export default SignIn;
---
+++ rate-repository-app/src/components/TextInput.jsx
@@ -0,0 +1,24 @@
+import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+    textInput: {
+        borderWidth: 2,
+        borderColor: theme.colors.textSecondary,
+        backgroundColor: "white",
+        padding: 5,
+        borderRadius: 5,
+        margin: 10
+    },
+    errorStyle: {
+        borderColor: "red"
+    }
+});
+
+const TextInput = ({ style, error, ...props }) => {
+    const textInputStyle = [styles.textInput, style, error && styles.errorStyle];
+
+    return <NativeTextInput style={textInputStyle} {...props} />;
+};
+
+export default TextInput;
